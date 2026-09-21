167cc78095a739a8bb5e0b0b926d709d099de281
DjMong5 <proff.clau@gmail.com>
2026-09-21 15:58:15 -0300

    Update PluginProcessor.h
    update

--- source/PluginProcessor.h
+++ source/PluginProcessor.h
@@ -1,1 +1,59 @@
+#pragma once
 
+#include <juce_audio_processors/juce_audio_processors.h>
+#include <juce_dsp/juce_dsp.h>
+
+class PluginProcessor : public juce::AudioProcessor
+{
+public:
+    PluginProcessor();
+    ~PluginProcessor() override;
+
+    void prepareToPlay (double sampleRate, int samplesPerBlock) override;
+    void releaseResources() override;
+
+   #ifndef JucePlugin_PreferredChannelConfigurations
+    bool isBusesLayoutSupported (const BusesLayout& layouts) const override;
+   #endif
+
+    void processBlock (juce::AudioBuffer<float>&, juce::MidiBuffer&) override;
+
+    juce::AudioProcessorEditor* createEditor() override;
+    bool hasEditor() const override;
+
+    const juce::String getName() const override;
+
+    bool acceptsMidi() const override;
+    bool producesMidi() const override;
+    bool isMidiEffect() const override;
+    double getTailLengthSeconds() const override;
+
+    int getNumPrograms() override;
+    int getCurrentProgram() override;
+    void setCurrentProgram (int index) override;
+    const juce::String getProgramName (int index) override;
+    void changeProgramName (int index, const juce::String& newName) override;
+
+    void getStateInformation (juce::MemoryBlock& destData) override;
+    void setStateInformation (const void* data, int sizeInBytes) override;
+
+    juce::AudioProcessorValueTreeState& getAPVTS() { return apvts; }
+
+private:
+    juce::AudioProcessorValueTreeState apvts;
+    juce::AudioProcessorValueTreeState::ParameterLayout createParameterLayout();
+
+    // DSP Components (Delay and Filters)
+    juce::dsp::DelayLine<float, juce::dsp::DelayLineInterpolationTypes::Linear> delayLineLeft;
+    juce::dsp::DelayLine<float, juce::dsp::DelayLineInterpolationTypes::Linear> delayLineRight;
+
+    juce::dsp::StateVariableTPTFilter<float> hpfLeft;
+    juce::dsp::StateVariableTPTFilter<float> hpfRight;
+    juce::dsp::StateVariableTPTFilter<float> lpfLeft;
+    juce::dsp::StateVariableTPTFilter<float> lpfRight;
+
+    float feedbackLeftSample = 0.0f;
+    float feedbackRightSample = 0.0f;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (PluginProcessor)
+};
